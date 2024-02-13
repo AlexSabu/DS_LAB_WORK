a577cbaba5d14d6e4c8c73acb9b6ef6beb437c56
AlexSabu <sabualex333@gmail.com>
2024-02-13 15:37:40 +0530

    changes made

--- linkedLoop.c
+++ linkedLoop.c
@@ -41,6 +41,18 @@ void loopHead() {
     loop->link=loop;
 }
 
+void createLoop(int data){
+    if(loop==NULL){
+        loopHead();
+    }
+    struct Node* new=createNode(data);
+    struct Node *current=loop;
+    while(current->link!=loop){
+        current=current->link;
+    }
+    current->link=new;
+    new->link=loop;
+}
 
 
 void check(){
