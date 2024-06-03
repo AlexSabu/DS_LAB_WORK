fa778efa0d361d48ab1494b87a10400cdc6060fd
AlexSabu <sabualex333@gmail.com>
2024-06-03 23:47:04 +0530

    changes made

--- graph.c
+++ graph.c
@@ -123,19 +123,7 @@ struct Graph* deleteEdge(struct Graph *G,int src,int dest){
     return G;
 }
 
-//DFS using recursion
-void DFS_rec(int vertex,int visited[]){
-    visited[vertex]=1;
-    printf("%d ->",vertex);
-    struct Node *current=graph->adjList[vertex];
-    while(current!=NULL){
-        int neighbour=current->data;
-        if(visited[neighbour]==0){
-            DFS_rec(neighbour,visited);
-        }
-        current=current->link;
-    }
-}
+
 
 
 //BFS 
